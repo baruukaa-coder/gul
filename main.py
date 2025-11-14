2490e22aa1a973daccea0bc75a5db88ee8ccafbb
baruukaa-coder <baruukaa@gmail.com>
2025-11-14 17:41:27 +0500

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from fastapi import FastAPI, HTTPException
 from pymongo import MongoClient
 import os
 
-app = FastAPI()  
+app = FastAPI()
 
 mongo_uri = os.environ.get("MONGO_URI")
 db_name = os.environ.get("DB_NAME")
@@ -10,10 +10,15 @@ db_name = os.environ.get("DB_NAME")
 client = MongoClient(mongo_uri)
 db = client[db_name]
 
+@app.get("/")
+def root():
+    return {"message": "API is running!"}
+
 @app.get("/health/db")
 def health_db():
     try:
-        db.list_collection_names()  
+        db.list_collection_names()
         return {"status": "ok", "db": db_name}
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
+
